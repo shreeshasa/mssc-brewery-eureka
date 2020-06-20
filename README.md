0930618815ecbe5754a341447c61bf6f14ea3fb1
Shreesha Adiga <ShreeshaShankar.Adiga@synchronoss.com>
2020-06-20 13:44:42 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# MSSC Brewery Eureka Service
---
+++ src/main/java/io/github/shreeshasa/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package io.github.shreeshasa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author shreeshasa
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+        .authorizeRequests()
+        .anyRequest().authenticated()
+        .and()
+        .httpBasic();
+  }
+}
